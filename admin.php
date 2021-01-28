43ea7c33e5d4b6d21e8a043a3e1eea34320e0a6b
StacyGru <stacy.gru@gmail.com>
2021-01-28 12:03:18 +0400

    Вход администратора через сессии, защита admin.php от доступа без авторизации

--- admin.php
+++ admin.php
@@ -1,4 +1,18 @@
 <?php
+    
+    echo "<link rel='stylesheet' href='style.css'>";
+
+    if (session_status() != 2)
+        session_start();
+    
+    if (!isset($_SESSION['username']))
+    {
+        echo 'Необходима авторизация';
+        exit;
+    }
+
+    echo '<a href="index.php/?logout=">Выход</a><br><br>';
+
     echo '<a href="/create.php" style="color: green;">Создать новую сессию</a><br>';
     echo '<a href="/close.php" style="color: red;">Закрыть одну из текущих сессий</a><br>';
     echo '<a href="/view.php" style="color: blue;">Просмотр информации по всем сессиям</a><br>';
--- index.php
+++ index.php
@@ -11,27 +11,51 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700&display=swap" rel="stylesheet">
         </head>
     <body>
+
     <?php
-        if (!isset($_GET['password']))
-            echo '<form>
-                    <label for="password" class="header">Введите пароль администратора ситемы:</label><br><br>
-                    <input type="password" id="password" name="password"></input><br>
-                    <input type="submit" name="enter" value="Войти">
-                    </form>';
-            
-        if (isset($_GET['password']) && $_GET['password'] == '12345')
-                include 'admin.php';
-        
-        if (isset($_GET['password']) && $_GET['password'] != '12345')
+
+        session_start();
+
+        if (isset($_GET['logout']))
         {
-            echo '<form>
+            session_destroy();  // удаляем инфу о пользователе
+            header('Location: /'); // перенаправление на главную страницу
+            exit();
+        }
+
+        if (!isset($_SESSION['username']) && !isset($_POST['password']))   // ЕСЛИ ВХОД ЕЩЁ НЕ ПРОИЗОШЁЛ И ДАННЫЕ ДЛЯ ВХОДА НЕ ПЕРЕДАНЫ
+        {
+            echo '<form name="auth" method="post" action="">
+                <label for="password" class="header">Введите пароль администратора ситемы:</label><br><br>
+                <input type="password" id="password" name="password"></input><br>
+                <input type="submit" name="enter" value="Войти">
+                </form>';   // выводим форму для входа
+        }
+
+        if (!isset($_SESSION['username']) && isset($_POST['password']))  // ЕСЛИ ВХОД ЕЩЁ НЕ ПРОИЗОШЁЛ, НО ДАННЫЕ ДЛЯ ВХОДА БЫЛИ ПЕРЕДАНЫ
+        {
+            if ($_POST['password'] == '12345')  // если пароль верный
+            {
+                $_SESSION['username'] = 'admin';
+                $_SESSION['password'] = '12345'; // начинаем сессию
+            }
+            else    // если пароль не верный
+            {
+                echo '<form name="auth" method="post" action="">
                     <label for="password" class="header">Введите пароль администратора ситемы:</label><br><br>
                     <input type="password" id="password" name="password"></input><br>
                     <input type="submit" name="enter" value="Войти">
-                    </form>';    
-            echo '<span style="color: red;">Неверный пароль!</span>';
+                    </form>';    // выводим форму
+                echo '<span style="color: red;">Неверный пароль!</span>';   // и сообщение об ошибке
+            }
         }
-        ?>
+
+        if (isset($_SESSION['username']))
+        {
+            include 'admin.php';    // подключаем функции администратора
+        }
+
+    ?>
         
         </body>
     </html>
